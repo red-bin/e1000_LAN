ed7e2f75eeabe36d9f4a0da4673bb9af5d42beb8
I am computer <user@iamcomputer.(none)>
2016-02-14 03:07:50 -0600

    Adding configs and continuation of scriptin (now with better oop)

--- planner.py
+++ planner.py
@@ -2,6 +2,7 @@
 
 from collections import defaultdict
 import itertools
+import numpy as np
 
 GIGETHBASE = """!
   interface GigabitEthernet %s/%s
@@ -14,66 +15,6 @@ interface Vlan %s
   ip address 10.0.%s.254/24
 %s
   no shutdown"""
-
-
-class Switch():
-    def __init__(self, switch_config):
-        self.vlans = self.create_vlans(switch_config)
-        #self.cards = self.create_linecards(switch_config)
-
-    def create_vlans(self, switch_config):
-        new_vlans = []
-        for vlan_conf in switch_config:
-            vlan_conf = vlan_conf.items()[0]
-            vlan_id, cards = vlan_conf
-            new_vlan = Vlan(vlan_id, cards)
-            new_vlans.append(new_vlan)
-
-        return new_vlans
-
-class Vlan():
-    def __init__(self, id, cards):
-        self.id = id
-        self.interfaces = self.create_interfaces(cards)
-        self.lines = self.create_vlan_lines()
-
-    def create_interfaces(self, cards):
-        interfaces = []
-        for card in cards:
-            card_id = card['card_id']
-            start,end = card['id_range']
-            id_range = range(start,end)
-
-            if_list = []
-            for id in id_range:
-                interface = Interface(id, card_id)
-                interfaces.append(interface)
-
-        return interfaces
-
-    def create_vlan_lines(self):
-        if_lines = self.get_if_vlanlines()
-        vlan_line = VLANBASE % (self.id, self.id, if_lines)
-        print vlan_line
-        return vlan_line
-         
-    def get_if_vlanlines(self):
-        ret_lines = [ iface.vlan_line for iface in self.interfaces ]
-        ret_lines = '\n'.join(ret_lines)
-
-        return ret_lines
-
-class Linecard():
-    def __init__(self, card_id, id_range):
-        self.card_id = card_id
-        self.id_range = range(*id_range)
-
-class Interface():
-    def __init__(self, id, card_id):
-        self.id = id
-        self.card_id = card_id
-        self.vlan_line = "  untagged GigabitEthernet %s/%s" % (card_id, id)
-        self.if_line = GIGETHBASE % (card_id, id)
 
 vlan_plan = [ 
     { 1: [ { 'card_id':1, 'id_range':(0,23) }] },
@@ -93,15 +34,97 @@ vlan_plan = [
                       { 'card_id':4, 'id_range':(0,17) }] },
     ]
 
-switch = Switch(vlan_plan)
-vlans = switch.vlans
+LINECARDLOC = "/opt/lan_e1000/LAN_e1000/configs/linecards.defs"
 
+class Lan(object):
+    def __init__(self, col_count=12, col_size=24):
+        self.switch = Switch(vlan_plan)
+        self.columns = self.create_columns(col_count, col_size)
+        print super(Lan,self)
 
-tmp_if = [ vlan.interfaces for vlan in vlans ]
-interfaces = itertools.chain(*tmp_if)
+    #def __setitem__(self, key, value):
+    #    print self.columns
 
-for i in interfaces:
-    print i.if_line
+    def create_columns(self, col_count, col_size):
+        col_range = range(1,col_count+1)
+        ret_cols = [ Column(id, col_size) for id in col_range ]
 
-for i in vlans:
-    print vlan.lines
+        return ret_cols
+        
+class Column(Lan):
+    def __init__(self, id, size):
+#        self.people = create_people(size)
+        print dir(super(Column, self).__init__())
+        self.vlan = id
+
+class Switch():
+    def __init__(self, switch_config):
+        self.vlans = self.create_vlans(switch_config)
+        self.cards = self.create_linecards(LINECARDLOC)
+
+    def create_vlans(self, switch_config):
+        new_vlans = []
+        for vlan_conf in switch_config:
+            vlan_conf = vlan_conf.items()[0]
+            vlan_id, cards = vlan_conf
+            new_vlan = Vlan(vlan_id, cards)
+            new_vlans.append(new_vlan)
+
+        return new_vlans
+
+    def create_linecards(self, conf_loc):
+        conf_lines = '\n'.join(open(conf_loc,'r').readlines())
+        exec(conf_lines) # linecards = { ........ }
+        for cardtype in linecards:
+            name,slots,size = cardtype.values()
+            cards = [ Linecard(name,slot,size) for slot in slots ]
+
+        linecards = itertools.chain(*cards)
+
+        return linecards
+
+class Vlan():
+    def __init__(self, id, cards):
+        self.id = id
+        self.lines = self.create_vlan_lines()
+
+    def create_vlan_lines(self):
+        if_lines = self.get_if_vlanlines()
+        vlan_line = VLANBASE % (self.id, self.id, if_lines)
+
+        return vlan_line
+         
+    def get_if_vlanlines(self):
+        #ret_lines = [ iface.vlan_line for iface in self.interfaces ]
+        #ret_lines = '\n'.join(ret_lines)
+        ret_lines = ''
+
+        return ret_lines
+
+class Linecard():
+    def __init__(self, name, slot, size):
+        self.name = name
+        self.slot = slot
+        self.size = size
+
+        self.interfaces = self.create_interfaces(self.size)
+
+    def create_interfaces(self, size):
+        interfaces = []
+        id_range = range(0,size)
+
+        if_list = []
+        for id in id_range:
+            interface = Interface(id)
+            interfaces.append(interface)
+
+        return interfaces
+
+class Interface():
+    def __init__(self, id):
+        self.id = id
+        self.vlan_line = "  untagged GigabitEthernet %s" % (id)
+        self.if_line = GIGETHBASE % (1, id)
+
+lan = Lan()
+#vlans = switch.vlans
